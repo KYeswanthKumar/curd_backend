8fe91330c760a9f1a56dbe349eb094dc1d39d803
KAKARLA YESWANTH KUMAR <163734820+KYeswanthKumar@users.noreply.github.com>
2025-08-05 12:18:06 +0530

    Update AppController.java

--- src/main/java/com/klu/AppController.java
+++ src/main/java/com/klu/AppController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,5 +38,5 @@ public class AppController {
 	public String delete(@PathVariable int id) {
 		return s.deleteData(id);
 	}
-	*/
+	
 }
